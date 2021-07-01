0ca4c9d87f2ee95ac0f3a70cb9b66ad1eaf83c53
pauliescanlon <pauliescanlon@gmail.com>
2021-07-01 11:33:43 +0100

    feat: add image

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,24 +3,30 @@ import { useStaticQuery, graphql } from 'gatsby';
 
 const IndexPage = () => {
   const {
-    apod: { id, date, title, explanation }
+    apod: { id, date, explanation, media_type, service_version, title, url }
   } = useStaticQuery(graphql`
     query {
       apod {
         id
         date
-        title
         explanation
+        media_type
+        service_version
+        title
+        url
       }
     }
   `);
 
   return (
     <main>
-      <small>{id}</small>
       <p>{date}</p>
       <h1>{title}</h1>
       <p>{explanation}</p>
+      <img alt={title} src={url} />
+      <p>{`id: ${id}`}</p>
+      <p>{`media_type: ${media_type}`}</p>
+      <p>{`service_version: ${service_version}`}</p>
     </main>
   );
 };
